5286858ceeedcddff20165422d566efbca9b45cb
Thomas John Helin <thomashelin@yahoo.com>
2020-04-09 22:00:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,6 +127,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] = "assembly", "C"
+programmer_hash[:dennis_ritchie][:languages] = "C", "Assembly"
 programmer_hash
 end
